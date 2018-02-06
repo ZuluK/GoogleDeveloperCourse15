512b8800b9cb2a3903d08d5826f626b052c9e61f
Richard Spence <richardspence@Richards-MacBook-Air-2.local>
2018-02-05 19:20:19 -0800

    First Commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+
+
+//Persistent data structures for efficient immutablity
+
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "ballroom";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (room === "dining room") {
+    weapon = "knife";
+    solved = true;
+    suspect = "Mr. Parkes";
+} else if (room === "gallery") {
+    weapon = "trophy"
+    solved = true;
+    suspect = "Ms. Van Cleve";
+} else if (room == "ballroom") {
+    weapon = "poison";
+    solved = true;
+    suspect = "Mr. Kalehoff";
+} else {
+    weapon = "pool stick";
+    solved = true;
+    suspect = "Mrs. Sparr";
+
+}
+
+if (solved) {
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
+
+
+/*
+ * Programming Quiz: Find my Seat (4-8)
+ *
+ * Write a nested for loop to print out all of the different seat combinations in the theater.
+ * The first row-seat combination should be 0-0
+ * The last row-seat combination will be 25-99
+ *
+ * Things to note:
+ *  - the row and seat numbers start at 0, not 1
+ *  - the highest seat number is 99, not 100
+ */
+
+// Write your code here
+for(var i = 0; i <= 25; i++){
+    for(var j = 0; j <= 99; j++){
+        console.log(i + "-" + j);
+    }
+}
